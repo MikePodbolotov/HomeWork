f60eb3a3b4a32f35aaebe067a1c0cfb1080b023b
PodM <m.a.podbolotov@gmail.com>
2020-11-19 00:08:37 +0300

    Solved task 3

--- 2l_ФИ.playground/Contents.swift
+++ 2l_ФИ.playground/Contents.swift
@@ -8,6 +8,7 @@ func evenNumber(number: Int) -> Bool {
 
 var result = evenNumber(number: 11)
 
+
 // 2. Написать функцию, которая определяет, делится ли число без остатка на 3.
 func division(number: Int, denominator: Int) -> Bool {
     let remainder = number % denominator
@@ -15,3 +16,15 @@ func division(number: Int, denominator: Int) -> Bool {
 }
 
 result = division(number: 12, denominator: 3)
+
+
+// 3. Создать возрастающий массив из 100 чисел.
+var someArrey = [Int]()
+var number = 0
+while someArrey.count < 100 {
+    someArrey.append(number)
+    number += 1
+}
+
+print(someArrey)
+
