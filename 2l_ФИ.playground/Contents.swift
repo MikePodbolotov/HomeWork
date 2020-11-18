2608466436f6d88aa4f37a579e03019bab559417
PodM <m.a.podbolotov@gmail.com>
2020-11-18 23:52:37 +0300

    Solved task 2

--- 2l_ФИ.playground/Contents.swift
+++ 2l_ФИ.playground/Contents.swift
@@ -6,5 +6,12 @@ func evenNumber(number: Int) -> Bool {
     return remainder == 0
 }
 
-let result = evenNumber(number: 11)
+var result = evenNumber(number: 11)
 
+// 2. Написать функцию, которая определяет, делится ли число без остатка на 3.
+func division(number: Int, denominator: Int) -> Bool {
+    let remainder = number % denominator
+    return remainder == 0
+}
+
+result = division(number: 12, denominator: 3)
