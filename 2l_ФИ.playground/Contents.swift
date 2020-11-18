475240b10edb09198325208e17b70d38c21e2adc
PodM <m.a.podbolotov@gmail.com>
2020-11-18 23:38:18 +0300

    Solved task 1

--- 2l_ФИ.playground/Contents.swift
+++ 2l_ФИ.playground/Contents.swift
@@ -1,1 +1,10 @@
+import Foundation
+
+// 1. Написать функцию, которая определяет, четное число или нет.
+func evenNumber(number: Int) -> Bool {
+    let remainder = number % 2
+    return remainder == 0
+}
+
+let result = evenNumber(number: 11)
 
